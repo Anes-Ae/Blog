ea89085ddfbc5630c02ae9dcc04122e764666ed9
Anes-Ae <AnesDev@gmail.com>
2025-05-26 13:42:21 +0330

    add admin setup for app core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields':('email', )})
+    )
+    fieldsets = UserAdmin.fieldsets
