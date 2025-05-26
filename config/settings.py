40126ca86ddc5d1cae50e77fe3fd8df85c7ac528
Anes-Ae <AnesDev@gmail.com>
2025-05-26 13:15:21 +0330

    set mysql settings

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':env("DB_NAME"),
+        'HOST':env("DB_HOST"),
+        'USER':env("DB_USER"),
+        'PASSWORD':env("DB_PASSWORD"),
     }
 }
 
