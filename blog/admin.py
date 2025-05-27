8a762e920f5d3027de75393b25c3890735f11fd0
Anes-Ae <AnesDev@gmail.com>
2025-05-27 16:12:32 +0330

    add post model admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+@admin.register(Post)
+class BlogAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = [
+        'title',
+        'author',
+        'status',
+        'is_featured',
+        'views',
+    ]
+    list_editable = [
+        'status',
+        'is_featured'
+    ]
+    readonly_fields = [
+        'created_at',
+        'updated_at',
+    ]
+    list_filter = [
+        'status',
+        'title',
+    ]
+    list_max_show_all = 10
+
