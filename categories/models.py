1dc4ff5e6b93f0e18d628d7e9ba965c30154711c
Anes-Ae <AnesDev@gmail.com>
2025-05-27 15:57:07 +0330

    add category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.utils.text import slugify, gettext_lazy as _
 
-# Create your models here.
+from taggit.managers import TaggableManager
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, related_name='child')
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.CharField(max_length=350)
+    image = models.ImageField(upload_to='category_images/')
+    tag = TaggableManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category-detail', args=[self.slug])
+
+
+    class Meta:
+        verbose_name = _('categories')
+        verbose_name_plural = _('category')
