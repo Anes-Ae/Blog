79884001ecbe36f0fd7ff565af633bcf83354b90
Anes-Ae <AnesDev@gmail.com>
2025-05-28 15:37:45 +0330

    clean up category admin.py

--- categories/admin.py
+++ categories/admin.py
@@ -4,14 +4,33 @@ from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at')
-    list_display_links = ('id', 'title')
-    search_fields = ('title', 'description')
-    list_filter = ('created_at', 'updated_at')
-    ordering = ('-created_at', 'title')
-    readonly_fields = [
-        'created_at', 'updated_at'
+    list_display = [
+        'id',
+        'title',
+        'created_at',
+        'updated_at',
     ]
+    list_display_links = [
+        'id',
+        'title',
+                          ]
+    search_fields = [
+        'title',
+        'description',
+    ]
+    list_filter = [
+        'created_at',
+        'updated_at',
+    ]
+    ordering = [
+        '-created_at',
+        'title',
+    ]
+    readonly_fields = [
+        'created_at',
+        'updated_at'
+    ]
+    prepopulated_fields = {'slug': ('title',)}
 
 
 
